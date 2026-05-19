87c7f347e68c254ecbf3c6f89a84ca5265f0146c
Victor <2402073@utrivieramaya.edu.mx>
2026-05-19 12:36:22 -0600

    inicio

---
+++ helloworld/src/views/HomeComponent.vue
@@ -0,0 +1,21 @@
+<!--Estructura SFC (3 partes: HTML, JS Y CSS)-->
+<script setup>
+//1.importaciones: otros componentes, servicios, composables
+
+import {ref, computed, onMouted} from 'vue'
+import {useWeatherStore} from '@/stores/weatherStore'
+
+//2. Estados locales - solo datos que este componente necesita
+
+const extra = ref(false);
+//3. Estado global- desde el store de piña
+
+const store = useWeatherStore()
+
+//4. Valores computados - derivados del estado de datos en nuestro store
+
+const iconClima = computed(()=>store.temperatura > 25? '-' : '-')
+
+//5. ciclo de vida
+
+</script>
